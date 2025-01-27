ab4c82872260e2bc869f45cba582c564c0e44bd5
Marcos <marcosvrsdevmail@gmail.com>
2025-01-27 09:13:32 -0300

    feat(Data Scrapper): adiciona o runner do scraper

--- live_scrapper/admin.py
+++ live_scrapper/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import LiveInfo
 
-# Register your models here.
+
+@admin.register(LiveInfo)
+class LiveInfoAdmin(admin.ModelAdmin):
+    list_display = ("id", "timestamp", "data_preview")  # Exibe os campos no painel
+    list_filter = ("timestamp",)  # Filtros laterais baseados na data
+    search_fields = ("data",)  # Permite buscar no campo de dados
+
+    def data_preview(self, obj):
+        # Retorna uma prévia dos dados JSON para exibição
+        return str(obj.data)[:75] + "..." if len(str(obj.data)) > 75 else str(obj.data)
+
+    data_preview.short_description = "Data Preview"
